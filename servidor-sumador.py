45f863485ae49a728fefb3369488d13e78118c15
cgomezcasa <cayegomez1995@gmail.com>
2018-03-01 10:26:16 +0100

    añadimos servidor v2

--- servidor-sumador.py
+++ servidor-sumador.py
@@ -18,15 +18,17 @@ try:
         try:
             res = resp.split()[1]
             _, op1, operation, op2 = res.split('/')
-            sol = calculadora.funciones[operation](float(op1),float(op2));
+            sol = calculadora.funciones[operation](float(op1), float(op2))
             answer = ("<p>Operacion: " + str(op1) + " " +
-                        operation + " " + str(op2) + " = " + str(sol) + "<p>")
+                      operation + " " + str(op2) + " = " + str(sol) + "<p>")
         except:
-            answer = ('<p>La entrada esta mal, vuelva a intentarlo.' +  
-                    'Recuerda que la entrada debe ser:' + 'localhost:1234/op1/operation/op2.<p>')
+            answer = ('<p>La entrada esta mal, vuelva a intentarlo.' +
+                      'Recuerda que la entrada debe ser:' +
+                      'localhost:1234/op1/operation/op2.<p>')
         recvSocket.send(bytes(
                         "HTTP/1.1 200 OK\r\n\r\n" +
-                        "<html><body><h1>Bienvenido.</h1>" + answer + "</body></html>" +
+                        "<html><body><h1>Bienvenido.</h1>" + answer +
+                        "</body></html>" +
                         "\r\n", "utf-8"))
         recvSocket.close()
 
